f696127c050623c8d3551100dc2ae6ecab6a07ed
kanha-jii <kanhasinghal0101@gmail.com>
2024-05-10 12:37:33 +0530

    food Activity

--- app/src/main/java/com/example/foodcollection/LoginPage.kt
+++ app/src/main/java/com/example/foodcollection/LoginPage.kt
@@ -3,6 +3,7 @@ package com.example.foodcollection
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
@@ -11,8 +12,14 @@ class LoginPage : AppCompatActivity() {
         setContentView(R.layout.activity_login_page)
 
         val signupMove =findViewById<TextView>(R.id.sign_tt3)
+        val loginButton = findViewById<Button>(R.id.login_bt)
+
         signupMove.setOnClickListener {
             val intent = Intent(this@LoginPage, MainActivity::class.java)
+            startActivity(intent)
+        }
+        loginButton.setOnClickListener {
+            val intent = Intent(this@LoginPage, FoodActivity::class.java)
             startActivity(intent)
         }
     }
