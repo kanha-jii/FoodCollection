01b425af408353992a120f0004e2dd45b0bd4e78
kanha-jii <kanhasinghal0101@gmail.com>
2024-05-10 12:33:55 +0530

    home activity

--- app/src/main/java/com/example/foodcollection/LoginPage.kt
+++ app/src/main/java/com/example/foodcollection/LoginPage.kt
@@ -1,11 +1,19 @@
 package com.example.foodcollection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val signupMove =findViewById<TextView>(R.id.sign_tt3)
+        signupMove.setOnClickListener {
+            val intent = Intent(this@LoginPage, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
